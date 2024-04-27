b8e9810d1bd21eac5eb2926d87b102c38eeb1716
XSerhiiX <avramserg1987@gmail.com>
2024-04-27 16:28:52 +0300

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,17 @@
-// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
-
-// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
-// Усі символи slug повинні бути в нижньому регістрі.
-// Усі слова slug повинні бути розділені тире.
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
-
-
-function slugify(title) {
-    return title.toLowerCase().split(' ').join('-');
+function checkFileExtension(fileName, ext) {
+   if (fileName.endsWith(ext) === true) {
+     return 'File extension matches';
+   } else {
+    return 'File extension does not match';
+   }
 }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+
+console.log(checkFileExtension("styles.css", ".css"));
+console.log(checkFileExtension("styles.css", ".js"));
+console.log(checkFileExtension("app.js", ".js"));
+console.log(checkFileExtension("app.js", ".html"));
+console.log(checkFileExtension("index.html", ".html"));
+console.log(checkFileExtension("index.html", ".css"));
+console.log(checkFileExtension("index.html", ".js"));
